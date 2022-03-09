a6ae20f47120b5445536351899640b760910a63b
yigitcankosar <40207929+yigitcankosar@users.noreply.github.com>
2022-03-09 20:44:56 +0300

    Event&Listener

---
+++ projem11/wwwroot/js/Student.js
@@ -0,0 +1,23 @@
+﻿
+document.getElementById('EklemeFormu').addEventListener('submit', function () {
+    document.getElementById('btnEkleme').innerHTML = 'Ekleniyor!';
+    document.getElementById('btnEkleme').disabled = 'Ekleniyor!';
+    
+});
+
+ 
+$('#NameAndSecondName').blur(function () {
+    if ($(this).val() == '') {
+        document.getElementById('lblNameAndSecondName').innerHTML = 'Boş Geçilemez!';
+    }
+    
+
+});
+
+$('#Mobile').blur(function () {
+    if ($(this).val().substring(0,1) != '5') {
+        document.getElementById('lblNameAndSecondName').innerHTML = 'Telefon 5 ile başlamalı!';
+    }
+
+
+});
